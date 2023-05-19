5c922afde6409ac84585fddca79055544bd17e1d
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-05-19 17:29:30 +0200

    change display

--- src/main/java/com/coolcode/Board.java
+++ src/main/java/com/coolcode/Board.java
@@ -42,67 +42,31 @@ public class Board {
         }
     }
 
-//        public void displayBoard() {
-//        for (int row = 0; row < size; row++) {
-//            for (int col = 0; col < size; col++) {
-//                Pawn pawn = fields[row][col];
-//                if (pawn != null) {
-//                    System.out.print(pawn.getXYPosition() + " ");
-//                } else {
-//                    System.out.print("-" + " ");
-//                }
-//            }
-//            System.out.println();
-//        }
-//    }
-//    public void displayBoard() {
-//        // print column labels
-//        System.out.print("  ");
-//        for (int col = 1; col <= size; col++) {
-//            System.out.print(col + " ");
-//        }
-//        System.out.println();
-//
-//        // print row labels and field contents
-//        for (int row = 0; row < size; row++) {
-//            // print row label
-//            System.out.print((char) ('A' + row) + " ");
-//
-//            // print field contents
-//            for (int col = 0; col < size; col++) {
-//                Pawn pawn = fields[row][col];
-//                if (pawn != null) {
-//                    System.out.print(pawn.getXYPosition() + " ");
-//                } else {
-//                    System.out.print("- ");
-//                }
-//            }
-//            System.out.println();
-//        }
-//    }
     public void displayBoard() {
-
         // print column labels
-        System.out.print(" ");
+        System.out.print("    ");
         for (int col = 1; col <= size; col++) {
-            System.out.print(col);
+            System.out.printf("%3d", col);
         }
         System.out.println();
 
         // print row labels and field contents
         for (int row = 0; row < size; row++) {
             // print row label
-            System.out.print((char) ('A' + row));
+            System.out.format("%4s", (char) ('A'  +  row));
 
 
             // print field contents
             for (int col = 0; col < size; col++) {
                 Pawn pawn = fields[row][col];
                 if (pawn != null) {
-                    System.out.print(getEmojiPawn(pawn.isWhite()));
-
+                    System.out.format("%3s", getEmojiPawn(pawn.isWhite()));
+                } else if(col % 2 != 0 && row % 2 == 0) {
+                    System.out.printf("%3s", "\u25A1");
+                } else if (col % 2 == 0 && row % 2 != 0) {
+                    System.out.printf("%3s", "\u25A1");
                 } else {
-                    System.out.print(" ");
+                    System.out.printf("%3s", "  ");
                 }
             }
             System.out.println();
@@ -113,6 +77,7 @@ public class Board {
     private String getEmojiPawn(boolean isWhite) {
         return isWhite ? "\u2659" : "\u265F";
     }
+
 
     public boolean isPanwOnFields(Coordinates coordinates){
         Pawn pawn = fields[coordinates.getX()][coordinates.getY()];
@@ -140,22 +105,6 @@ public class Board {
         System.out.print(" ");
     }
 
-
-//    public static int convertStringToInteger(String letter) {
-//        HashMap<String, Integer> columnsDict = new HashMap<>();
-//        columnsDict.put("A", 1);
-//        columnsDict.put("B", 1);
-//        columnsDict.put("C", 2);
-//        columnsDict.put("D", 3);
-//        columnsDict.put("E", 4);
-//        columnsDict.put("F", 5);
-//        columnsDict.put("G", 6);
-//        columnsDict.put("H", 7);
-//        columnsDict.put("I", 8);
-//        columnsDict.put("J", 9);
-//
-//        return columnsDict.get(String.valueOf(letter.charAt(0)));
-//    }
 
     public Pawn[][] getFields() {
         return fields;
--- src/main/java/com/coolcode/Main.java
+++ src/main/java/com/coolcode/Main.java
@@ -8,16 +8,3 @@ public class Main {
         game.start();
     }
 }
-
-
-
-//        // TODO: get user input and check it try except
-//
-//        Board board2 = new Board(17);
-//
-//        System.out.println(board2.getSize());
-//
-//        board2.displayBoard();
-//
-//    }
-//}
